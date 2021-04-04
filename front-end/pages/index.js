1cef5ab5d828dfd4ba83284f077e9c72b67cfa6b
kitravee <kitravee.swk@gmail.com>
2021-04-04 15:28:44 +0700

    Add - _app and Homepage

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,65 +1,44 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { Row, Col } from 'react-bootstrap';
+import PageLayout from 'src/components/PageLayout';
+import CardItem from 'src/components/CardItem';
 
-export default function Home() {
+import { getAllBlogs } from 'lib/api';
+import useGetBlogs from 'src/hooks/useGetBlogs';
+
+export default function Home({ blogs: initialData }) {
+  const { data: blogs, error } = useGetBlogs(initialData);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <PageLayout>
+      <Row className="mb-5">
+        {blogs.map((blog) => (
+          <Col key={blog.slug} md="4">
+            <CardItem
+              author={blog.author}
+              date={blog.date}
+              image={blog.coverImage}
+              link={{
+                href: '/blogs/[slug]',
+                as: `/blogs/${blog.slug}`,
+              }}
+              subtitle={blog.subtitle}
+              title={blog.title}
+            />
+          </Col>
+        ))}
+      </Row>
+    </PageLayout>
   );
+}
+
+//? This function is called during the build (build time)
+//? Provides props to your page
+//? It will create static page
+export async function getStaticProps() {
+  const blogs = await getAllBlogs({ offset: 0 });
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
