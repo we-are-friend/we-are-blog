99b5013694f9002ecc5231effc90358199f46786
kitravee <kitravee.swk@gmail.com>
2021-04-11 02:59:11 +0700

    Add - PageLayout

--- front-end/src/components/PageLayout.jsx
+++ front-end/src/components/PageLayout.jsx
@@ -1,17 +1,9 @@
 import { Container } from 'react-bootstrap';
 import Navbar from './Navbar';
-import Head from 'next/head';
 
 export default function PageLayout({ children, className }) {
   return (
     <>
-      <Head>
-        <link href="https://fonts.gstatic.com" rel="preconnect" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500&family=Barlow:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Navbar />
       <Container>
         <div className={`page-wrapper ${className}`}>{children}</div>
