336d36b9488263543644e91b689123a02a4cf80c
Natta <nattarintra.thutsanti@hotmail.com>
2021-04-11 13:34:36 +0200

    merge footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
-import { createStyles, makeStyles } from '@material-ui/core';
+import { createStyles, makeStyles, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     root: {},
+    title: {
+      marginBottom: theme.spacing(1.5),
+      fontFamily: 'Barlow Condensed',
+      fontSize: 36,
+      fontStyle: 'normal',
+      fontWeight: 800,
+      lineHeight: '43.2px',
+      letterSpacing: 0.25,
+      //textAlign: 'left',
+    },
+    subtitle: {
+      marginBottom: theme.spacing(2),
+    },
   }),
 );
 const Footer = ({ className }) => {
   const classes = useStyles();
   return (
-    <footer className={clsx(classes.root, className)}>
-      <a href="#">Footer</a>
+    <footer align="center" className={clsx(classes.root, className)}>
+      <Typography align="center" className={classes.title} color="primary">
+        LOGO-LOGO-LOGO
+      </Typography>
+      <Typography className={classes.subtitle} variant="body1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, hic.
+      </Typography>
+
+      <Typography variant="overline">
+        &copy; {new Date().getFullYear()} We Are Friend Co. All rights reserved.
+      </Typography>
     </footer>
   );
 };
