31fdda29950d1de92c5e750a267c4e75507e1155
kitravee <kitravee.swk@gmail.com>
2021-04-17 02:10:39 +0700

    Fix - Card Component

--- front-end/pages/index.jsx
+++ front-end/pages/index.jsx
@@ -1,32 +1,33 @@
-import { Row, Col } from 'react-bootstrap';
 import PageLayout from 'src/components/PageLayout';
-import CardItem from 'src/components/CardItem';
-
 import { getAllBlogs } from 'lib/api';
 import useGetBlogs from 'src/hooks/useGetBlogs';
+import { Grid } from '@material-ui/core';
+import VerticalCard from 'src/components/VerticalCard';
 
 export default function Home({ blogs: initialData }) {
   const { data: blogs } = useGetBlogs(initialData);
 
   return (
     <PageLayout>
-      <Row className="mb-5">
-        {blogs.map((blog) => (
-          <Col key={blog.slug} md="4">
-            <CardItem
-              author={blog.author}
-              date={blog.date}
-              image={blog.coverImage}
-              link={{
-                href: '/blogs/[slug]',
-                as: `/blogs/${blog.slug}`,
-              }}
-              subtitle={blog.subtitle}
-              title={blog.title}
-            />
-          </Col>
-        ))}
-      </Row>
+      <Grid container justify="center">
+        <Grid container item spacing={3} xs={10}>
+          {blogs.map((blog) => (
+            <Grid key={blog.slug} item xs={4}>
+              <VerticalCard
+                author={blog.author}
+                date={blog.date}
+                image={blog.coverImage}
+                link={{
+                  href: '/blogs/[slug]',
+                  as: `/blogs/${blog.slug}`,
+                }}
+                subtitle={blog.subtitle}
+                title={blog.title}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
     </PageLayout>
   );
 }
