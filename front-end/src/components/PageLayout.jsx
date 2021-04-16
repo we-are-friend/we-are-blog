9559ef7b77546b568dccb8761cc3304efa6a9d2e
kitravee <kitravee.swk@gmail.com>
2021-04-16 18:43:06 +0700

    Fix - Page Layout

--- front-end/src/components/PageLayout.jsx
+++ front-end/src/components/PageLayout.jsx
@@ -1,14 +1,13 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { Container } from '@material-ui/core';
 
 export default function PageLayout({ children, className }) {
   return (
     <>
       <Navbar />
-      <Container>
-        <div className={`page-wrapper ${className}`}>{children}</div>
-      </Container>
+
+      <div className={`page-wrapper ${className}`}>{children}</div>
+
       <Footer />
     </>
   );
