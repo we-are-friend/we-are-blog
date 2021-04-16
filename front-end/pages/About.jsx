add82400757c3a90c27fceb467f5c81cd1824069
kitravee <kitravee.swk@gmail.com>
2021-04-16 17:42:49 +0700

    Add - Banner to About Page

--- front-end/pages/About.jsx
+++ front-end/pages/About.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import clsx from 'clsx';
-import { createStyles, makeStyles } from '@material-ui/core';
 import PageLayout from 'src/components/PageLayout';
 import Banner from 'src/components/Banner';
-import CustomButton from 'src/components/CustomButton';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {},
-  }),
-);
-const About = ({ className }) => {
-  const classes = useStyles();
+const About = () => {
   return (
     <PageLayout>
-      <div className={clsx(classes.root, className)}>
-        About we-are-friend
-        <Banner />
-        <CustomButton>Hi</CustomButton>
-      </div>
+      <Banner />
     </PageLayout>
   );
 };
