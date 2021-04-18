b6e60bb6025652fe399842fa27050d92c45b9242
kitravee <kitravee.swk@gmail.com>
2021-04-18 15:26:16 +0700

    Create - Nav and Sidebar

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -2,3 +2,10 @@ export const sanityConfig = {
   SANITY_DATASET_NAME: process.env.SANITY_DATASET_NAME,
   SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
 };
+export const navContent = [
+  { content: 'HOME', link: '/' },
+  { content: 'ABOUT', link: '/About' },
+  { content: 'STORYTELLER', link: '/About' },
+  { content: 'CHAPTER', link: '/About' },
+  { content: 'CONTACT', link: '/About' },
+];
